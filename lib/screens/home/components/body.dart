52c7049c2ecc8692daae59428132ba4838c21801
valehasadli <valehasadli@gmail.com>
2020-11-26 17:15:45 +0400

    feat: build search text field

--- lib/screens/home/components/body.dart
+++ lib/screens/home/components/body.dart
@@ -10,41 +10,41 @@ class Body extends StatelessWidget {
       child: SingleChildScrollView(
         child: Column(
           children: [
-            buildSearch(),
+            Padding(
+              padding: EdgeInsets.symmetric(
+                horizontal: getProportionateScreenWidth(20),
+              ),
+              child: Row(
+                children: [
+                  buildSearch(),
+                ],
+              ),
+            ),
           ],
         ),
       ),
     );
   }
 
-  Padding buildSearch() {
-    return Padding(
-      padding: EdgeInsets.symmetric(
-        horizontal: getProportionateScreenWidth(20),
+  Container buildSearch() {
+    return Container(
+      width: SizeConfig.screenWidth * 0.6,
+      decoration: BoxDecoration(
+        color: kSecondaryColor.withOpacity(0.1),
+        borderRadius: BorderRadius.circular(15),
       ),
-      child: Row(
-        children: [
-          Container(
-            width: SizeConfig.screenWidth * 0.6,
-            decoration: BoxDecoration(
-              color: kSecondaryColor.withOpacity(0.1),
-              borderRadius: BorderRadius.circular(15),
-            ),
-            child: TextField(
-              onChanged: (String value) {},
-              decoration: InputDecoration(
-                enabledBorder: InputBorder.none,
-                focusedBorder: InputBorder.none,
-                hintText: 'Search Product',
-                prefixIcon: Icon(Icons.search),
-                contentPadding: EdgeInsets.symmetric(
-                  horizontal: getProportionateScreenWidth(20),
-                  vertical: getProportionateScreenWidth(9),
-                ),
-              ),
-            ),
+      child: TextField(
+        onChanged: (String value) {},
+        decoration: InputDecoration(
+          enabledBorder: InputBorder.none,
+          focusedBorder: InputBorder.none,
+          hintText: 'Search Product',
+          prefixIcon: Icon(Icons.search),
+          contentPadding: EdgeInsets.symmetric(
+            horizontal: getProportionateScreenWidth(20),
+            vertical: getProportionateScreenWidth(9),
           ),
-        ],
+        ),
       ),
     );
   }
