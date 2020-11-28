ab01d6fa742c7fb3e59f363b1af9d851ba1a0f5f
valehasadli <valehasadli@gmail.com>
2020-11-28 19:48:34 +0400

    feat: checkout card

--- lib/screens/cart/cart_screen.dart
+++ lib/screens/cart/cart_screen.dart
@@ -2,8 +2,10 @@ import 'package:flutter/material.dart';
 
 import '../../size_config.dart';
 import '../../constants.dart';
+import '../../models/cart_model.dart';
 
 import 'components/body.dart';
+import 'components/check_out_card.dart';
 
 class CartScreen extends StatelessWidget {
   static const String routeName = '/cart';
@@ -13,7 +15,28 @@ class CartScreen extends StatelessWidget {
     SizeConfig().init(context);
 
     return Scaffold(
+      appBar: buildAppBar(context),
       body: Body(),
+      bottomNavigationBar: CheckoutCard(),
+    );
+  }
+
+  AppBar buildAppBar(BuildContext context) {
+    return AppBar(
+      title: Column(
+        children: [
+          Text(
+            'Your Cart',
+            style: TextStyle(
+              color: Colors.black,
+            ),
+          ),
+          Text(
+            '${demoCarts.length} item(s)',
+            style: Theme.of(context).textTheme.caption,
+          )
+        ],
+      ),
     );
   }
 }
